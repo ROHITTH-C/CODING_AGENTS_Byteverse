2f089c77fc24f5af2b7d59401bd4d18d7a92b0d4
ROHITTH.C <chennupallir.ug23.cs@nitp.ac.in>
2024-04-14 11:32:42 +0530

    mid evalution
    uploaded for mid evalution

---
+++ coding_agents/home.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="style.css" rel="stylesheet" />
+</head>
+<body>
+    
+<img class="imgtp" src="imgtop.jpg">
+<p class ="head">Learn More</p>                                   
+<div class="button">
+    <a href="login.html"><div class="text">LOG IN / RESCUE</div></a>
+    </div>
+ <div class="topbody">  
+     <h1 class="cen">The community in a box</h1>
+    <p class="cen">The perfect Stop to save Animals at yours finger tips.</p>
+    <img class="i1"src="img1.jpg">
+<div class="adj">
+<div class="events"><a href="post.php"><div class="hi"><strong>Post an emergency</strong></div></a>
+    <p class="stron">post a picture of an animal which needs help</p>
+</div>
+<div class="spaces"><a href="rescue.php"><div class="hi"><strong>RESCUE</strong></div></a>
+    <p class="stron">You can help the animals in your surroundings</p>
+</div>
+    <div class="hi"><strong>Donate</strong></div>
+    <p class="stron"><br>You can donate any food or fund to animal rescue teams</p>
+</div>
+</div>
+</body>
+</html>
